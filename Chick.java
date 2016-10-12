1bb07e0a3ff92c5ddc053d7c4b0aca9992b620aa
garvingit <garvinmo@gmail.com>
2016-10-12 11:34:39 -0700

    2

--- Chick.java
+++ Chick.java
@@ -1,4 +1,16 @@
-class Chick //implements Animal 
-{     
-     //your code here
+class Chick implements Animal {     
+     private String myType;     
+     private String mySound;      
+     public Chick(String type, String sound, String sound2)     {         
+         myType = type;         
+         if(Math.random > .5)  
+         mySound = sound;
+         else mySound = sound2;   
+     }     
+     public Chick()     {         
+         myType = "unknown";         
+         mySound = "unknown";     
+     }      
+     public String getSound(){return mySound;}     
+     public String getType(){return myType;} 
 }
--- OldMacDonald.pde
+++ OldMacDonald.pde
@@ -1,12 +1,6 @@
-
 void setup()
 {
-	Cow mo = new cow();
-	//your variable declarations here
-	Animal[] herd = new Animal[2];
-	Animal[0] = mo;
-	System.out.println(Animal[0].getType() + "goes " + Animal[0].getsSound());
-
-
+	Farm herd = new Farm();
+	herd.animalSounds();
 }
 
